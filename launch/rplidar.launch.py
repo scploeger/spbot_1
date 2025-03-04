5cc3f8afd92d7da9596478fe0b7bc92b5627631b
scploeger <spenceinvents@hotmail.com>
2025-03-03 21:57:39 -0500

    modified launchfile

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
             executable='rplidar_node',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4.4:1.0-port0',
                 'serial_baudrate': 115200,
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,

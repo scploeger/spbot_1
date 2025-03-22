1f9aea25a154620657342940bb4f4d4d44c7ab2b
spencer <spencer@sploeger.com>
2025-03-22 18:20:16 -0400

    created launchfile to launch robot control with camera and lidar included

---
+++ launch/launch_robot_complete.launch.py
@@ -0,0 +1,99 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+
+    package_name='spbot_1'
+    
+    # use_ros2_control = LaunchConfiguration('use_ros2_control', default='true')
+
+    # launch robot state publisher
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    )
+    
+    # use a command substitution to get URDF
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    
+    # get the controller params file
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+
+    # spawn the controller manager
+    # need to pass params: URDF - so hardware interface can be loaded, params file - so correct controllers can be loaded
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description}, controller_params_file]
+    )
+    
+    # make sure the RSP starts before the controller manager
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    
+    # after we spawn the robot, we want to start the controller manager for diff_cont and joint_broad
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+    
+    # make sure this wait until controller manager is up
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+    
+    # make sure this wait until controller manager is up
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+    
+    camera_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'camera.launch.py'
+        )])
+    )
+    
+    lidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rplidar.launch.py'
+        )])
+    )
+
+    # Launch rsp, gazebo, spawn the robot
+    return LaunchDescription([
+        rsp,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
+        camera_launch,
+        lidar_launch
+    ])
